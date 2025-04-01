f30f69c25a5032489f7deda66d04e8aee2fa0fa3
sanakang0615 <sanakang0615@kaist.ac.kr>
2025-04-01 15:21:19 -0400

    Add the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-      extend: {},
+      extend: {
+        fontFamily: {
+          grotesk: ['"Space Grotesk"', 'sans-serif'],
+          dm: ['"DM Sans"', 'sans-serif'],
+        },
+      },
     },
     plugins: [],
   }
